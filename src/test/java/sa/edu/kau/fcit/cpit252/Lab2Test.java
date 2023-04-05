af91ded4cba53fc33dd943ab1803b05e8a002188
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-04-05 17:38:06 +0300

    add test cases

--- src/test/java/sa/edu/kau/fcit/cpit252/Lab2Test.java
+++ src/test/java/sa/edu/kau/fcit/cpit252/Lab2Test.java
@@ -3,7 +3,11 @@ package sa.edu.kau.fcit.cpit252;
 import org.junit.Test;
 import sa.edu.kau.fcit.cpit252.logging.Logger;
 
-import static org.junit.Assert.assertEquals;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.*;
 
 public class Lab2Test {
 
@@ -14,4 +18,27 @@ public class Lab2Test {
         Logger log2 = Logger.getLoggerInstance();
         assertEquals(log1, log2);
     }
+
+    // We should not have access to the constructor as it should be private
+    @Test
+    public void ShouldBePrivateConstructor() {
+        for (Constructor c : Logger.class.getConstructors()) {
+            assertTrue(Modifier.isPrivate(c.getModifiers()));
+        }
+    }
+
+    // We should have a private and static global variable that holds a reference to the singleton instance.
+    @Test
+    public void shouldBeStatic(){
+        boolean hasLoggerVar = false;
+        for (Field f : Logger.class.getDeclaredFields()){
+            if(f.getType() == Logger.class){
+                hasLoggerVar = true;
+                assertTrue(Modifier.isPrivate(f.getModifiers()));
+                assertTrue(Modifier.isStatic(f.getModifiers()));
+            }
+        }
+        assertTrue(hasLoggerVar);
+    }
+
 }
