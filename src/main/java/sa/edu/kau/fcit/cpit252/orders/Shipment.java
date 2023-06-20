474ab911244cd9e6057928dbc7f11275c86dc825
moayad201 <98517589+moayad201@users.noreply.github.com>
2023-06-20 12:54:03 +0300

    Update Shipment.java

--- src/main/java/sa/edu/kau/fcit/cpit252/orders/Shipment.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/orders/Shipment.java
@@ -9,7 +9,7 @@ public class Shipment {
     private String name;
     private String address;
     private String phoneNumber;
-    private Logger log = new Logger();
+    private Logger log = Logger.getLoggerInstance();
 
     private int getRandomNumber() {
         Random ran = new Random();
@@ -36,5 +36,4 @@ public class Shipment {
         return "Shipment info:\nTracking number: " + this.trackingNumber +
                 "\nName" + this.name + "\nAddress: " + this.address +
                 "\nPhone: " + this.phoneNumber;
-    }
-}
+}}
